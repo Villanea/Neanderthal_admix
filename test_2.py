bf8b926c9e9847bd4c97c9613de22d2de904c9c9
Villanea <fervillanea@gmail.com>
2017-02-21 13:05:13 -0500

    replaced append /n with join /n

--- test_2.py
+++ test_2.py
@@ -38,7 +38,6 @@ def neanderthal_admixture_model(num_modern=1000,anc_pop = 1, anc_num = 1, anc_ti
 	sim_num = 0	
 	for sim in sims:
 		for tree in sim.trees():
-			
 			cur_node = len(samples)-1  # the very last leaf, when adding more modern pops make sure Neanderthal is still last
 			while tree.get_time(tree.get_parent(cur_node)) < split_time:
 				cur_node = tree.get_parent(cur_node)
@@ -49,12 +48,11 @@ def neanderthal_admixture_model(num_modern=1000,anc_pop = 1, anc_num = 1, anc_ti
 				length[-1] = F_length
 				break
 			else:			
-				length.append(F_length)
 				freq.append(N_freq)
+				length.append(F_length)
 				data.append(N_freq)
-				data.append(",")
 				data.append(F_length)
-				data.append("\n")
+				'\n'.join(data)
 	del freq[0] #the first 0 prevents the very first loop from crashing
 	outfile = open('outfile.csv', 'w')
 	outfile.write("frequency,length")
