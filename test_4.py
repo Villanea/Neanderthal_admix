cfab5fe790eda106c57efd27ed57f804daa621e9
Fernando Villanea <fernandovillanea@78-4f-43-7e-57-23.wireless.temple.edu>
2017-02-24 11:28:36 -0500

    first working version of window SNP matching for recording freq of introgression

--- test_4.py
+++ test_4.py
@@ -4,6 +4,7 @@
 
 import msprime as msp
 import numpy as np
+import random
 #based on Schraiber Admixture model: https://github.com/Schraiber/continuity/blob/master/ancient_genotypes.py
 class FreqError(Exception):
 	pass
@@ -18,7 +19,7 @@ class FreqError(Exception):
 		
 #TODO: loop to automate between windows and replicates
 		
-def neanderthal_admixture_model(num_modern=1000,anc_pop = 1, anc_num = 1, anc_time=900,mix_time=1000,split_time=12000,f=0.03,Ne0=10000,Ne1=2500,mu=1.5e-8,rho=1.0e-8,length=10000000,window_size = 1000000,number_sites = 1,num_rep=100,coverage=False):
+def neanderthal_admixture_model(num_modern=1000,anc_pop = 1, anc_num = 1, anc_time=900,mix_time=1000,split_time=12000,f=0.03,Ne0=10000,Ne1=2500,mu=1.5e-8,rho=1.0e-8,length=10000000,window_size = 1000000,num_SNP = 1,num_rep=100,coverage=False):
 	#when is best time to sample Neanderthal? 100 gen before f?
 	#error catching, leave there for now
 	if f < 0 or f > 1:
@@ -33,48 +34,42 @@ def neanderthal_admixture_model(num_modern=1000,anc_pop = 1, anc_num = 1, anc_ti
 	win = []
 	freq = []
 	leng = []
-	interval = [] #probs don't need to store the interval
-	#window_size & number_sites
 	#FYI mean fragment length from test_2 model ~6000 bp
 	sim_num = 0	
 	cur_start = 0
 	cur_end = window_size-1
-	windows = [i]*(length/window_size)
+	windows = [0]*(length/window_size)
 	cur_win = 1
 	for sim in sims:
-		#select (number_sites) random position
 		for window in windows
-			
-			cur_site = random.randint(cur_start,cur_end)
 			for tree in sim.trees():
 				cur_node = len(samples)-1  #the very last leaf, when adding more modern pops make sure Neanderthal is still last
 				while tree.get_time(tree.get_parent(cur_node)) < split_time:
 					cur_node = tree.get_parent(cur_node)
 				F_length = tree.get_length()
 				N_freq = (tree.get_num_leaves(cur_node) - 1) #minus our lone Neanderthal
-				F_int = tree.get_interval(cur_node)
-				interval.append(F_int)
-				cur_win = range(cur_start,cur_end)
-				if cur_site in range star-to-end of fragment, record frequency
-					win.append(cur_win)
-					freq.append(N_freq)
-					leng.append(F_length)
-					
+				F_int = tree.get_interval() #if code works optimize by inserting inside loop
+				#interval.append(F_int)
+				for i in range (num_SNP): #populates list of SNP positions
+					cur_site = random.randint(cur_start,cur_end)
+					if cur_site in range(F_int):
+						win.append(cur_win)
+						freq.append(N_freq)
+						leng.append(F_length)
 			cur_start += window_size
 			cur_end += window_size
 			cur_win += 1
-			
-			
-			
 	outfile = open('outfile.csv', 'w')
-	outfile.write("frequency,length")
+	outfile.write("window,frequency,length")
 	outfile.write('\n')
 	for line in range(0,len(leng)):
+		outfile.write(str(win[line]))
+		outfile.write(',')
 		outfile.write(str(freq[line]))
 		outfile.write(',')
 		outfile.write(str(leng[line]))
 		outfile.write('\n')
 	outfile.close()
-	return np.array(freq), np.array(leng)
+	return np.array(win), np.array(freq), np.array(leng)
 
 N_admix = neanderthal_admixture_model()
