668775966eee14abd5700bbe90c0e56601b0fe8d
Fernando Villanea <fernandovillanea@78-4f-43-7e-57-23.wireless.temple.edu>
2017-03-16 11:13:15 -0400

    this is the first commit of the dilution model

---
+++ n_admix_04.py
@@ -0,0 +1,97 @@
+import msprime as msp
+import numpy as np
+import random
+
+import sys
+
+#this is the dilution model
+
+#Sim parameters from Moorjani et al 2016
+#Modern humans Ne 10000, samples 10
+#Neanderthal Ne 2500, samples 0
+#mu 1.5e-8 bp/gen
+#rho 1.0e-8 bp/gen
+#split time_1 12000 gen
+#split time_2 1500 gen
+#f1 0.02
+#f2 0.01
+#f1 time 2000 gen
+#f2 time 1000 gen
+#eu=european pop 1, as=asian pop 2, ba=basaleur pop 0, neand pop 3		
+
+		
+def neanderthal_admixture_model(num_eu=100,num_as=100,num_nean = 1,anc_time=900,mix_time1=2000,mix_time3=1000,split_time_1=120000,split_time_2=1500,f1=0.03,f3=0.01,Ne0=10000,Ne1=2500,mu=1.5e-8,rho=1.0e-8,length=10000000,window_size = 1000000,num_SNP = 1,num_rep=1000,coverage=False): #when is best time to sample Neanderthal? 100 gen before f?
+	if f1 < 0 or f1 > 1 or f2 < 0 or f2 > 1: #error catching, leave there for now
+		print "Admixture fraction is not in [0,1]"
+		return None
+	samples = [msp.Sample(population=1,time=0)]*num_eu
+	samples.extend([msp.Sample(population=2,time=0)]*num_as) #no sampling of Basal Eurasian pop
+	samples.extend([msp.Sample(population=3,time=anc_time)]*(num_nean)) #sample 1 Neanderthal for comparison
+	pop_config = [msp.PopulationConfiguration(initial_size=Ne0),msp.PopulationConfiguration(initial_size=Ne0),msp.PopulationConfiguration(initial_size=Ne0),msp.PopulationConfiguration(initial_size=Ne1)]
+	divergence = [msp.MassMigration(time=mix_time3,source=0,destination=1,proportion = f3), #BE dilution into EU
+			msp.MassMigration(time=split_time_2,source=2,destination=1,proportion=1.0), #EU AS split
+			msp.MassMigration(time=mix_time1,source=3,destination=1,proportion = f1), #first pulse
+			msp.MassMigration(time=split_time_1,source=2,destination=1,proportion=1.0)] # Neand EU split
+	sims = msp.simulate(samples=samples,Ne=Ne0,population_configurations=pop_config,demographic_events=divergence,mutation_rate=mu,recombination_rate=rho,length=length,num_replicates=num_rep)
+	print "done simulating"
+	win = []
+	freq_EU = []
+	freq_AS = []
+	leng = []
+	cur_sim = 0
+	for sim in sims:
+		cur_win = 1
+		cur_start = 0
+		cur_end = window_size-1
+		cur_site = (cur_start+cur_end)/2.0 #random.randint(cur_start,cur_end)
+		cur_sim += 1
+		print "current simulation"
+		print cur_sim
+		for tree in sim.trees():
+			F_int = tree.get_interval()
+			if cur_site >= F_int[0] and cur_site < F_int[1]:
+				#raw_input()
+				cur_node = len(samples)-1  #the very last leaf, when adding more modern pops make sure Neanderthal is still last
+				while tree.get_time(tree.get_parent(cur_node)) < split_time_1:
+					cur_node = tree.get_parent(cur_node)
+				F_length = tree.get_length()
+				N_freq_EU = 0
+				N_freq_AS = 0
+				for leaf in tree.leaves(cur_node):
+					if tree.get_population(leaf)== 0:
+						N_freq_EU += 1
+					elif tree.get_population(leaf) == 1:
+						N_freq_AS += 1
+				win.append(cur_win)
+				freq_EU.append(N_freq_EU)
+				freq_AS.append(N_freq_AS)
+				leng.append(F_length)
+				cur_start += window_size
+				cur_end += window_size
+				if cur_end > length:
+					break
+				cur_win += 1
+				#print cur_win
+				cur_site = (cur_start+cur_end)/2.0 #random.randint(cur_start,cur_end)
+	outfile = open('outfile_be.txt', 'w')
+	outfile.write("window\tfrequency_EU\tfrequency_AS\tlength")
+	outfile.write('\n')
+	for line in range(0,len(leng)):
+		outfile.write(str(win[line]))
+		outfile.write('\t')
+		outfile.write(str(freq_EU[line]))
+		outfile.write('\t')
+		outfile.write(str(freq_AS[line]))
+		outfile.write('\t')
+		outfile.write(str(leng[line]))
+		outfile.write('\n')
+	outfile.close()
+	return np.array(win), np.array(freq_EU), np.array(freq_AS), np.array(leng)
+
+num_rep = 1
+window_size = 100000
+if len(sys.argv) > 1: 
+	num_rep = int(sys.argv[1]) # take some command line arguments
+if len(sys.argv) > 2:
+	window_size = int(sys.argv[2]) # take some command line arguments
+N_admix = neanderthal_admixture_model(window_size = window_size, num_rep = num_rep,f2=0)
