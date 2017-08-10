ae68d8ebbee858102c337a5f4429b50973febdab
Josh Schraiber <jgschraiber@gmail.com>
2017-08-10 17:16:41 -0400

    added errors to marginal models

--- sym_stat_theory.py
+++ sym_stat_theory.py
@@ -34,21 +34,31 @@ def generate_het_dilution(f1,f2,n):
 def generate_het_two(f1,f2,n):
 	return generate_het(f2+(1-f2)*f1,n)
 
-def one_pulse(f,t,n):
+def one_pulse(f,t,n, error = 0):
 	h = generate_het_one(f,n)
 	Q = generate_Q_het(n)
 	sample = expma(Q*t,h)
 	comb = sp.binom(n,np.arange(0,n+1))
-	return comb*sample
+	sample *= comb
+	if error:
+		for i in range(len(sample)):
+			error_prob = st.binom.pmf(n=range(i,n+1),k=range(n-i+1), p=error)
+			sample[i] = np.dot(sample[i:],error_prob)
+	return sample
 
-def two_pulse(f1,t1,f2,t2,n):
+def two_pulse(f1,t1,f2,t2,n, error = 0):
 	h = generate_het_two(f1,f2,n)
 	Q = generate_Q_het(n)
 	Qm = generate_Qm_het(n)
 	sample = expma((Q-f2*Qm)*t1,h)
 	sample = expma(Q*t2,sample)
 	comb = sp.binom(n,np.arange(0,n+1))
-	return comb*sample
+	sample *= comb
+	if error:
+		for i in range(len(sample)):
+			error_prob = st.binom.pmf(n=range(i,n+1),k=range(n-i+1), p=error)
+			sample[i] = np.dot(sample[i:],error_prob)
+	return sample
 
 def dilution(f1,t1,f2,t2,n):
 	h = generate_het_dilution(f1,f2,n)
@@ -83,11 +93,47 @@ def sym_stat_dilution(f1,f2,tp,ta,te1,te2,n,norm=False):
 	print f1,f2,tp,ta,te1,te2
 	return sym
 
+def sym_stat_two_two(f1,fa,fe,tp,ta1,ta2,te1,te2,n,norm=False):
+	t1AS = tp+ta1
+	t2AS = ta2
+	t1EU = tp+te1
+	t2EU = te2
+	freqEU = two_pulse(f1,t1EU,fe,t2EU,n)
+	freqAS = two_pulse(f1,t1AS,fa,t2AS,n)
+	if norm:
+		freqEU = freqEU[1:]/sum(freqEU[1:])
+		freqAS = freqAS[1:]/sum(freqAS[1:])
+	sym = (freqEU-freqAS)/(freqEU+freqAS+1)
+	print f1,fa,fe,tp,ta1,ta2,te1,te2
+	return sym
+
 #NB: DON'T INCLUDE THE ZERO CATEGORY!!!!
 def fit_two_pulse(dat):
 	n = len(dat)
-	return opt.curve_fit(lambda x, f1,f2,tp,ta1,ta2,te: sym_stat_two_pulse(f1,f2,tp,ta1,ta2,te,n,True), 1, dat, p0 = st.uniform.rvs(size=6), bounds=((0,0,0,0,0,0),(1,1,1,1,1,1)))
+	return opt.curve_fit(lambda x, f1,f2,tp,ta1,ta2,te: sym_stat_two_pulse(f1,f2,tp,ta1,ta2,te,n,True), 1, dat, p0 = st.uniform.rvs(size=6,scale=.5), bounds=((0,0,0,0,0,0),(1,1,.5,.5,.5,.5)))
 
 def fit_dilution(dat):
 	n = len(dat)
-	return opt.curve_fit(lambda x, f1,f2,tp,ta,te1,te2: sym_stat_dilution(f1,f2,tp,ta,te1,te2,n,True), 1, dat, p0 = st.uniform.rvs(size=6), bounds=((0,0,0,0,0,0),(1,1,1,1,1,1)))
+	return opt.curve_fit(lambda x, f1,f2,tp,ta,te1,te2: sym_stat_dilution(f1,f2,tp,ta,te1,te2,n,True), 1, dat, p0 = st.uniform.rvs(size=6,scale=.5), bounds=((0,0,0,0,0,0),(.5,.8,.5,.5,.5,.5)))#,ftol=1e-10,gtol=1e-10)
+
+def fit_two_two(dat):
+	n = len(dat)
+	return opt.curve_fit(lambda x, f1,fa,fe,tp,ta1,ta2,te1,te2: sym_stat_two_two(f1,fa,fe,tp,ta1,ta2,te1,te2,n,True), 1, dat, p0 = st.uniform.rvs(size=8,scale=.5), bounds=((0,0,0,0,0,0,0,0),(.5,.5,.5,.5,.5,.5,.5,.5)))#,ftol=1e-10,gtol=1e-10)
+
+def one_like(dat, f, t,start=1, error = 0):
+	n = len(dat)-1
+	dat = dat[start:]
+	theory = one_pulse(f,t,n,error=error)[start:]
+	theory = theory/np.sum(theory)
+	lnL = np.sum(dat*np.log(theory))
+	print f, t, error,  lnL
+	return lnL
+
+def two_like(dat, f1, t1, f2, t2, start = 1, error = 0):
+	n = len(dat) - 1
+	dat = dat[start:]
+	theory = two_pulse(f1,t1,f2,t2,n, error=error)[start:]
+	theory = theory/np.sum(theory)
+	lnL = np.sum(dat*np.log(theory))
+	print f1, t1, f2, t2, lnL
+	return lnL
