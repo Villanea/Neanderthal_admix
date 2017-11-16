b1d96f3eb7604d1df857cb723eff928b61247faa
Villanea <fervillanea@gmail.com>
2017-11-16 14:40:02 -0500

    fixed bugs

--- n_admix_10.py
+++ n_admix_10.py
@@ -30,6 +30,7 @@ from joblib import Parallel, delayed
 
 #TODO: Compartmentalize all the pieces into functions, add random seed generator for ID into the parameter feeding loop
 def sim_pipeline(m1,m2,m3,m4,t1,t2,t3,f1,f2,f3,f4,w,n,ID):
+	print(ID)
 	outfile = open('outfile_sim%s.bed' %(ID), 'w+')
 	outfile.close()
 	def neanderthal_admixture_model(num_eu=170,num_as=394,num_nean = 1,anc_time=900,mix_time1=2000,mix_time2=1000,mix_time3=1000,mix_time4=1000,split_time_1=120000,split_time_2=2300,split_time_3=1500,f1=0.022,f2=0.00,f3=0.00,f4=0.20,Ne0=10000,Ne1=2500,Ne2=10000,mu=1.5e-8,window_size = 100000,num_SNP = 1,num_rep=1,coverage=False, seed=ID):
@@ -103,7 +104,7 @@ def sim_pipeline(m1,m2,m3,m4,t1,t2,t3,f1,f2,f3,f4,w,n,ID):
 			outfile.close()
 		return np.array(pos), np.array(pos1), np.array(freq_EU), np.array(freq_AS)
 
-	N_admix = neanderthal_admixture_model(mix_time1=m1,mix_time2=m2,mix_time3=m3,mix_time4=m4,split_time_1=t1,split_time_2=t2,split_time_3=t3,f1=f1,f2=f2,f3=f3,f4=f4,window_size =w,num_rep=n, ID=ID)
+	N_admix = neanderthal_admixture_model(mix_time1=m1,mix_time2=m2,mix_time3=m3,mix_time4=m4,split_time_1=t1,split_time_2=t2,split_time_3=t3,f1=f1,f2=f2,f3=f3,f4=f4,window_size =w,num_rep=n, seed=ID)
 
 	#bedops
 	def bedops():
@@ -180,7 +181,7 @@ def sim_pipeline(m1,m2,m3,m4,t1,t2,t3,f1,f2,f3,f4,w,n,ID):
 		outfile.write('\t')
 		outfile.write(str(m4))
 		outfile.write('\t')
-	O_file = outfile(m1,m2,m3,m4,t1,t2,t3,f1,f2,f3,f4,ID):
+	O_file = outfile(m1,m2,m3,m4,t1,t2,t3,f1,f2,f3,f4,ID)
 	
 	def outmatrix(EU_AS_pd):
 		outmatrix = open('symmetry_matrix_%s' %(ID), 'w+')
@@ -188,4 +189,4 @@ def sim_pipeline(m1,m2,m3,m4,t1,t2,t3,f1,f2,f3,f4,w,n,ID):
 		outmatrix.close()
 	O_matrix = outmatrix(EU_AS_pd)
 
-Sim = sim_pipeline(m1=2000,m2=1000,m3=1000,m4=1000,t1=12000,t2=2300,t3=1500,f1=0.022,f2=0.01,f3=0.01,f4=0.20,w=100000,n=1,ID = random.randint(1000000,size=1)
+Sim = sim_pipeline(m1=2000,m2=1000,m3=1000,m4=1000,t1=12000,t2=2300,t3=1500,f1=0.022,f2=0.01,f3=0.01,f4=0.20,w=100000,n=1,ID = random.randint(1,1000000))
