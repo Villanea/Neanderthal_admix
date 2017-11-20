1cbc3c0c6916a98287927f5aeeb95c72c6bc7655
Fernando Villanea <fernandovillanea@78-4f-43-7e-57-23.wireless.temple.edu>
2017-11-20 13:25:22 -0500

    N_admix inside main function

--- n_admix_10.py
+++ n_admix_10.py
@@ -30,76 +30,6 @@ from joblib import Parallel, delayed
 
 #TODO: Compartmentalize all the pieces into functions, add random seed generator for ID into the parameter feeding loop
 
-def neanderthal_admixture_model(seed,num_eu=170,num_as=394,num_nean = 1,anc_time=900,mix_time1=2000,mix_time2=1000,mix_time3=1000,mix_time4=1000,split_time_1=120000,split_time_2=2300,split_time_3=1500,f1=0.022,f2=0.00,f3=0.00,f4=0.20,Ne0=10000,Ne1=2500,Ne2=10000,mu=1.5e-8,window_size = 100000,num_SNP = 1,num_rep=1,coverage=False):
-		for chr in range(1,23):
-			infile = "/mnt/md0/villanea/MSprime/chr%s_map" %(chr)
-			rho_map = msp.RecombinationMap.read_hapmap(infile)
-			samples = [msp.Sample(population=0,time=0)]*num_eu
-			samples.extend([msp.Sample(population=1,time=0)]*num_as) #no sampling of Basal Eurasian pop
-			samples.extend([msp.Sample(population=3,time=anc_time)]*(num_nean)) #sample 1 Neanderthal for comparison
-			pop_config = [msp.PopulationConfiguration(initial_size=Ne0),msp.PopulationConfiguration(initial_size=Ne0),msp.PopulationConfiguration(initial_size=Ne0),msp.PopulationConfiguration(initial_size=Ne1)]
-			divergence = [msp.MassMigration(time=mix_time4,source=0,destination=2,proportion = f4), #BE dilution into EU
-					msp.MassMigration(time=mix_time3,source=0,destination=3,proportion = f3), #second pulse EU
-					msp.MassMigration(time=mix_time2,source=1,destination=3,proportion = f2), #second pulse AS
-					msp.MassMigration(time=split_time_3,source=0,destination=1,proportion=1.0), #EU AS split
-					msp.MassMigration(time=mix_time1,source=1,destination=3,proportion = f1), #first pulse
-					msp.MassMigration(time=split_time_2,source=1,destination=2,proportion=1.0), #BE AS split
-					msp.MassMigration(time=split_time_1,source=3,destination=2,proportion=1.0)] # Neand AS split
-			sims = msp.simulate(samples=samples,Ne=Ne0,population_configurations=pop_config,demographic_events=divergence,mutation_rate=mu,recombination_map=rho_map,num_replicates=num_rep)
-			chrom = "chr%s" %(chr)
-			pos = []
-			pos1 = []
-			freq_EU = []
-			freq_AS = []
-			last = np.array(rho_map.get_positions()[-1])
-			cur_sim = 0
-			for sim in sims:
-				cur_win = 1
-				cur_start = 0
-				cur_end = window_size-1
-				cur_site = int(((cur_start+cur_end)+1)/2.0)
-				cur_sim += 1
-				for tree in sim.trees():
-					F_int = tree.get_interval()
-					while cur_site >= F_int[0] and cur_site < F_int[1]:
-						cur_node = len(samples)-1  #the very last leaf, when adding more modern pops make sure Neanderthal is still last
-						while tree.get_time(tree.get_parent(cur_node)) < split_time_1:
-							cur_node = tree.get_parent(cur_node)
-						N_freq_EU = 0
-						N_freq_AS = 0
-						for leaf in tree.leaves(cur_node):
-							if tree.get_population(leaf)== 0:
-								N_freq_EU += 1
-							elif tree.get_population(leaf) == 1:
-								N_freq_AS += 1
-						pos.append(cur_site)
-						pos1.append(cur_site+1)
-						freq_EU.append(N_freq_EU)
-						freq_AS.append(N_freq_AS)
-						cur_start += window_size
-						cur_end += window_size
-						#print cur_end
-						#print last
-						if cur_end > last:
-							break
-						cur_win += 1
-						#print cur_win
-						cur_site = int(((cur_start+cur_end)+1)/2.0) #random.randint(cur_start,cur_end)
-						print cur_site
-			outfile = open('outfile_sim%s.bed' %(ID), 'a')
-			for line in range(0,len(freq_AS)):
-				outfile.write(chrom)
-				outfile.write('\t')
-				outfile.write(str(pos[line]))
-				outfile.write('\t')
-				outfile.write(str(pos1[line]))
-				outfile.write('\t')
-				outfile.write(str(freq_EU[line]))
-				outfile.write('\t')
-				outfile.write(str(freq_AS[line]))
-				outfile.write('\n')
-			outfile.close()
-		return np.array(pos), np.array(pos1), np.array(freq_EU), np.array(freq_AS)
 
 def bedops():
 	os.system("sort-bed outfile_sim%s.bed > outfile_sim%s_sorted.bed" %(ID,ID))
@@ -181,7 +111,77 @@ def sim_pipeline(m1,m2,m3,m4,t1,t2,t3,f1,f2,f3,f4,w,n,ID):
 	print(ID)
 	outfile = open('outfile_sim%s.bed' %(ID), 'w+')
 	outfile.close()
-	
+	def neanderthal_admixture_model(seed=1,num_eu=170,num_as=394,num_nean = 1,anc_time=900,mix_time1=2000,mix_time2=1000,mix_time3=1000,mix_time4=1000,split_time_1=120000,split_time_2=2300,split_time_3=1500,f1=0.022,f2=0.00,f3=0.00,f4=0.20,Ne0=10000,Ne1=2500,Ne2=10000,mu=1.5e-8,window_size = 100000,num_SNP = 1,num_rep=1,coverage=False):
+		for chr in range(1,23):
+			infile = "/mnt/md0/villanea/MSprime/chr%s_map" %(chr)
+			rho_map = msp.RecombinationMap.read_hapmap(infile)
+			samples = [msp.Sample(population=0,time=0)]*num_eu
+			samples.extend([msp.Sample(population=1,time=0)]*num_as) #no sampling of Basal Eurasian pop
+			samples.extend([msp.Sample(population=3,time=anc_time)]*(num_nean)) #sample 1 Neanderthal for comparison
+			pop_config = [msp.PopulationConfiguration(initial_size=Ne0),msp.PopulationConfiguration(initial_size=Ne0),msp.PopulationConfiguration(initial_size=Ne0),msp.PopulationConfiguration(initial_size=Ne1)]
+			divergence = [msp.MassMigration(time=mix_time4,source=0,destination=2,proportion = f4), #BE dilution into EU
+					msp.MassMigration(time=mix_time3,source=0,destination=3,proportion = f3), #second pulse EU
+					msp.MassMigration(time=mix_time2,source=1,destination=3,proportion = f2), #second pulse AS
+					msp.MassMigration(time=split_time_3,source=0,destination=1,proportion=1.0), #EU AS split
+					msp.MassMigration(time=mix_time1,source=1,destination=3,proportion = f1), #first pulse
+					msp.MassMigration(time=split_time_2,source=1,destination=2,proportion=1.0), #BE AS split
+					msp.MassMigration(time=split_time_1,source=3,destination=2,proportion=1.0)] # Neand AS split
+			sims = msp.simulate(samples=samples,Ne=Ne0,population_configurations=pop_config,demographic_events=divergence,mutation_rate=mu,recombination_map=rho_map,num_replicates=num_rep)
+			chrom = "chr%s" %(chr)
+			pos = []
+			pos1 = []
+			freq_EU = []
+			freq_AS = []
+			last = np.array(rho_map.get_positions()[-1])
+			cur_sim = 0
+			for sim in sims:
+				cur_win = 1
+				cur_start = 0
+				cur_end = window_size-1
+				cur_site = int(((cur_start+cur_end)+1)/2.0)
+				cur_sim += 1
+				for tree in sim.trees():
+					F_int = tree.get_interval()
+					while cur_site >= F_int[0] and cur_site < F_int[1]:
+						cur_node = len(samples)-1  #the very last leaf, when adding more modern pops make sure Neanderthal is still last
+						while tree.get_time(tree.get_parent(cur_node)) < split_time_1:
+							cur_node = tree.get_parent(cur_node)
+						N_freq_EU = 0
+						N_freq_AS = 0
+						for leaf in tree.leaves(cur_node):
+							if tree.get_population(leaf)== 0:
+								N_freq_EU += 1
+							elif tree.get_population(leaf) == 1:
+								N_freq_AS += 1
+						pos.append(cur_site)
+						pos1.append(cur_site+1)
+						freq_EU.append(N_freq_EU)
+						freq_AS.append(N_freq_AS)
+						cur_start += window_size
+						cur_end += window_size
+						#print cur_end
+						#print last
+						if cur_end > last:
+							break
+						cur_win += 1
+						#print cur_win
+						cur_site = int(((cur_start+cur_end)+1)/2.0) #random.randint(cur_start,cur_end)
+						print cur_site
+			outfile = open('outfile_sim%s.bed' %(ID), 'a')
+			for line in range(0,len(freq_AS)):
+				outfile.write(chrom)
+				outfile.write('\t')
+				outfile.write(str(pos[line]))
+				outfile.write('\t')
+				outfile.write(str(pos1[line]))
+				outfile.write('\t')
+				outfile.write(str(freq_EU[line]))
+				outfile.write('\t')
+				outfile.write(str(freq_AS[line]))
+				outfile.write('\n')
+			outfile.close()
+		return np.array(pos), np.array(pos1), np.array(freq_EU), np.array(freq_AS)
+
 	N_admix = neanderthal_admixture_model(seed=ID,mix_time1=m1,mix_time2=m2,mix_time3=m3,mix_time4=m4,split_time_1=t1,split_time_2=t2,split_time_3=t3,f1=f1,f2=f2,f3=f3,f4=f4,window_size =w,num_rep=n)
 
 	#bedops
