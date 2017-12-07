6f7880afc95954c337fb2d32432a222ea95ae55f
Villanea <fervillanea@gmail.com>
2017-12-07 13:23:08 -0500

    set for 1000 reps, one pulse model

--- n_admix_10.py
+++ n_admix_10.py
@@ -181,27 +181,29 @@ def ofile(ID,m1,m2,m3,m4,t1,t2,t3,f1,f2,f3,f4,Ne0,Ne1,Ne3,Ne4):
 #Ne4 AS_EU ancestral pop Ne
 #EU=european pop 0, AS=asian pop 1, BE=basaleur pop 2, Nean pop 3    
 
-ID = np.random.randint(1,100000000,size=2)
-m1 = scipy.stats.uniform.rvs(loc=1500, scale=1500, size=2)
+num_reps=1000
+
+ID = np.random.randint(1,100000000,size=num_reps)
+m1 = scipy.stats.uniform.rvs(loc=1500, scale=1500, size=num_reps)
 t_bound = 2000
-t3 = scipy.stats.uniform.rvs(loc=1300, scale=(np.minimum(t_bound,m1)-1300), size=2)
+t3 = scipy.stats.uniform.rvs(loc=1300, scale=(np.minimum(t_bound,m1)-1300), size=num_reps)
 m_bound = 2000
-m2 = scipy.stats.uniform.rvs(loc=800, scale=(np.minimum(m_bound,t3)-800), size=2)
-m3 = scipy.stats.uniform.rvs(loc=800, scale=(np.minimum(m_bound,t3)-800), size=2)
-m4 = scipy.stats.uniform.rvs(loc=200, scale=(np.minimum(m_bound,t3)-200), size=2)
-f1 = scipy.stats.uniform.rvs(loc=0, scale=0.1, size=2)
-f2 = scipy.stats.uniform.rvs(loc=0, scale=0.1, size=2)
-f3 = scipy.stats.uniform.rvs(loc=0, scale=0.1, size=2)
-f4 = scipy.stats.uniform.rvs(loc=0, scale=0.5, size=2)
-Ne0 = np.rint(scipy.stats.uniform.rvs(loc=5000, scale=95000, size=2))
+m2 = scipy.stats.uniform.rvs(loc=800, scale=(np.minimum(m_bound,t3)-800), size=num_reps)
+m3 = scipy.stats.uniform.rvs(loc=800, scale=(np.minimum(m_bound,t3)-800), size=num_reps)
+m4 = scipy.stats.uniform.rvs(loc=200, scale=(np.minimum(m_bound,t3)-200), size=num_reps)
+f1 = scipy.stats.uniform.rvs(loc=0, scale=0.1, size=num_reps)
+#f2 = scipy.stats.uniform.rvs(loc=0, scale=0.1, size=num_reps)
+#f3 = scipy.stats.uniform.rvs(loc=0, scale=0.1, size=num_reps)
+#f4 = scipy.stats.uniform.rvs(loc=0, scale=0.5, size=num_reps)
+Ne0 = np.rint(scipy.stats.uniform.rvs(loc=5000, scale=95000, size=num_reps))
 print(Ne0)
-Ne1 = np.rint(scipy.stats.uniform.rvs(loc=5000, scale=95000, size=2))
+Ne1 = np.rint(scipy.stats.uniform.rvs(loc=5000, scale=95000, size=num_reps))
 print(Ne1)
-Ne3 = np.rint(scipy.stats.uniform.rvs(loc=500, scale=4500, size=2))
+Ne3 = np.rint(scipy.stats.uniform.rvs(loc=500, scale=4500, size=num_reps))
 print(Ne3)
-Ne4 = np.rint(scipy.stats.uniform.rvs(loc=5000, scale=45000, size=2))
+Ne4 = np.rint(scipy.stats.uniform.rvs(loc=5000, scale=45000, size=num_reps))
 print(Ne4)
 
 #Sim = Parallel(n_jobs=2)(delayed(sim_pipeline)(ID[i],m1=2000,m2=1000,m3=1000,m4=1000,t1=12000,t2=2300,t3=1500,f1=0.022,f2=0.01,f3=0.01,f4=0.2,Ne0=10000,Ne1=10000,Ne3=2500,Ne4=10000,w=100000,n=1) for i in range(2))
 
-Sim = Parallel(n_jobs=2)(delayed(sim_pipeline)(ID[i],m1=m1[i],m2=m2[i],m3=m3[i],m4=m4[i],t1=10000,t2=3000,t3=t3[i],f1=f1[i],f2=f2[i],f3=f3[i],f4=f4[i],Ne0=Ne0[i],Ne1=Ne1[i],Ne3=Ne3[i],Ne4=Ne4[i],w=100000,n=1) for i in range(2))
+Sim = Parallel(n_jobs=50)(delayed(sim_pipeline)(ID[i],m1=m1[i],m2=m2[i],m3=m3[i],m4=m4[i],t1=10000,t2=3000,t3=t3[i],f1=f1[i],f2=0,f3=0,f4=0,Ne0=Ne0[i],Ne1=Ne1[i],Ne3=Ne3[i],Ne4=Ne4[i],w=100000,n=1) for i in range(num_reps))
